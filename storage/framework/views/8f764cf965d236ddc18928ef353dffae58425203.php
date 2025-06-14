ff58701cf1a08238dfccf5ecd82e53c62f39ac3b
wayu1st <139676118+wayu1st@users.noreply.github.com>
2025-06-14 22:45:02 +0800

    Add files via upload

---
+++ storage/framework/views/8f764cf965d236ddc18928ef353dffae58425203.php
@@ -0,0 +1,23 @@
+<form wire:submit.prevent="authenticate" class="space-y-8">
+    <?php echo e($this->form); ?>
+
+
+    <?php if (isset($component)) { $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4 = $component; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'filament::components.button','data' => ['type' => 'submit','form' => 'authenticate','class' => 'w-full']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? (array) $attributes->getIterator() : [])); ?>
+<?php $component->withName('filament::button'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag && $constructor = (new ReflectionClass(Illuminate\View\AnonymousComponent::class))->getConstructor()): ?>
+<?php $attributes = $attributes->except(collect($constructor->getParameters())->map->getName()->all()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['type' => 'submit','form' => 'authenticate','class' => 'w-full']); ?>
+        <?php echo e(__('filament::login.buttons.submit.label')); ?>
+
+     <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4)): ?>
+<?php $component = $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4; ?>
+<?php unset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4); ?>
+<?php endif; ?>
+</form>
+<?php /**PATH D:\manajemen-surat\vendor\filament\filament\src\/../resources/views/login.blade.php ENDPATH**/ ?>
